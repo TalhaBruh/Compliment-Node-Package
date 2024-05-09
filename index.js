4392ad7b65c9a0cd3910de2c1725f7b7168080db
Talha Hussain <talhahussain349@gmail.com>
2024-05-10 00:03:24 +0500

    Npm package publish

---
+++ index.js
@@ -0,0 +1,22 @@
+// Define an array of compliments
+const compliments = [
+    "You're as bright as a shooting star on a clear night!",
+    "Your smile could light up the darkest room!",
+    "You're a ray of sunshine on a cloudy day!",
+    "You're as awesome as a double rainbow after a storm!",
+    "You're more fun than a bubble bath full of rubber ducks!",
+    "You're one in a million!",
+    "You're the bee's knees!",
+    "You're as cool as a cucumber!",
+    "You're sweeter than a basket of puppies!",
+    "You're as smart as Einstein!",
+];
+
+// Function to generate a random compliment
+function generateCompliment() {
+    const randomIndex = Math.floor(Math.random() * compliments.length);
+    return compliments[randomIndex];
+}
+
+// Export the function so it can be used by other modules
+module.exports = { generateCompliment };
