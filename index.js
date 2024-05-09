cc140eef10f93366f009f801de877e175f621ea6
Talha Hussain <talhahussain349@gmail.com>
2024-05-10 00:32:51 +0500

    major Reset Functionality added

--- index.js
+++ index.js
@@ -23,5 +23,23 @@ function addCompliment(newCompliment) {
     compliments.push(newCompliment);
 }
 
+// Function to reset compliments to default
+function resetCompliments() {
+    compliments = [
+        "You're as bright as a shooting star on a clear night!",
+        "Your smile could light up the darkest room!",
+        "You're a ray of sunshine on a cloudy day!",
+        "You're as awesome as a double rainbow after a storm!",
+        "You're more fun than a bubble bath full of rubber ducks!",
+        "You're one in a million!",
+        "You're the bee's knees!",
+        "You're as cool as a cucumber!",
+        "You're sweeter than a basket of puppies!",
+        "You're as smart as Einstein!",
+        // Add more default compliments here
+    ];
+}
+
+
 // Export the function so it can be used by other modules
 module.exports = { generateCompliment };
