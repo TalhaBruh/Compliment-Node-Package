970fe951e747b49c67e653431a81c0da5c1f5a1f
Talha Hussain <talhahussain349@gmail.com>
2024-05-10 00:38:00 +0500

    Custom Language functionality added

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-// Define an array of compliments
-const compliments = [
-    "You're as bright as a shooting star on a clear night!",
-    "Your smile could light up the darkest room!",
-    "You're a ray of sunshine on a cloudy day!",
-    "You're as awesome as a double rainbow after a storm!",
-    "You're more fun than a bubble bath full of rubber ducks!",
-    "You're one in a million!",
-    "You're the bee's knees!",
-    "You're as cool as a cucumber!",
-    "You're sweeter than a basket of puppies!",
-    "You're as smart as Einstein!",
-];
+// Define an object containing compliments in different languages
+const complimentsByLanguage = {
+    english: [
+        "You're as bright as a shooting star on a clear night!",
+        "Your smile could light up the darkest room!",
+        "You're a ray of sunshine on a cloudy day!",
+        "You're as awesome as a double rainbow after a storm!",
+        "You're more fun than a bubble bath full of rubber ducks!",
+        "You're one in a million!",
+        "You're the bee's knees!",
+        "You're as cool as a cucumber!",
+        "You're sweeter than a basket of puppies!",
+        "You're as smart as Einstein!",
+        // Add more English compliments here
+    ],
+    spanish: [
+        "¡Eres tan brillante como una estrella fugaz en una noche clara!",
+        "¡Tu sonrisa podría iluminar la habitación más oscura!",
+        "¡Eres un rayo de sol en un día nublado!",
+        // Add more Spanish compliments here
+    ],
+    french: [
+        "Tu es aussi lumineux qu'une étoile filante par une nuit claire!",
+        "Ton sourire pourrait illuminer la pièce la plus sombre!",
+        "Tu es un rayon de soleil par une journée nuageuse!",
+        // Add more French compliments here
+    ],
+    // Add more languages and their compliments as needed
+};
 
-// Function to generate a random compliment
-function generateCompliment() {
-    const randomIndex = Math.floor(Math.random() * compliments.length);
-    return compliments[randomIndex];
+// Default language
+let currentLanguage = 'english';
+
+// Function to generate a random compliment in a specified language
+function generateCompliment(language = currentLanguage) {
+    const languageCompliments = complimentsByLanguage[language.toLowerCase()];
+    if (!languageCompliments) {
+        throw new Error(`Language '${language}' is not supported.`);
+    }
+    const randomIndex = Math.floor(Math.random() * languageCompliments.length);
+    return languageCompliments[randomIndex];
 }
 
 // Function to add a custom compliment
@@ -40,6 +63,13 @@ function resetCompliments() {
     ];
 }
 
+// Function to set the current language
+function setLanguage(language) {
+    if (!complimentsByLanguage[language.toLowerCase()]) {
+        throw new Error(`Language '${language}' is not supported.`);
+    }
+    currentLanguage = language.toLowerCase();
+}
 
-// Export the function so it can be used by other modules
-module.exports = { generateCompliment };
+// Export the functions so they can be used by other modules
+module.exports = { generateCompliment, addCompliment, resetCompliments, setLanguage };
